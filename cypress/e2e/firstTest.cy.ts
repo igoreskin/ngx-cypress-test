708436c03e9c9d10f5702b4a2fa24afd363ef755
Igor Eskin <ieskin@drivewealth.tech>
2022-11-19 16:05:03 -0500

    lists and dropdowns

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -149,8 +149,30 @@ describe('our first suite', () => {
 
   it.only('lists and dropdowns', () => {
     cy.visit('/')
-
+    // 1 - verifying one item of the dropdown 
     cy.get('nav nb-select').click()
     cy.get('.options-list').contains('Dark').click()
+    cy.get('nav nb-select').should('contain', 'Dark')
+    cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+    // 2 - iterating through all the items of the dropdown 
+    cy.get('nav nb-select').then(dropdown => {
+      cy.wrap(dropdown).click()
+      cy.get('.options-list nb-option').each((listItem, index) => {
+        const itemText = listItem.text().trim()
+
+        const colors = {
+          'Light': 'rgb(255, 255, 255)',
+          'Dark': 'rgb(34, 43, 69)',
+          'Cosmic': 'rgb(50, 50, 89)',
+          'Corporate': 'rgb(255, 255, 255)',
+        }
+
+        cy.wrap(listItem).click()
+        cy.wrap(dropdown).should('contain', itemText)
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+        index < 3 && cy.wrap(dropdown).click()
+      })
+    })
   })
 })
